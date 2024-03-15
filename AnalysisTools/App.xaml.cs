6fe448d220f97c096c6ed6b3af4ca426df31285e
钟斯琦 <1713182863@qq.com>
2024-03-15 21:08:28 +0800

    初始化

---
+++ AnalysisTools/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AnalysisTools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ AnalysisTools/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalysisTools
+{
+    public partial class MainWindowViewModel
+    {
+        private void Submit()
+        {
+
+        }
+    }
+}
